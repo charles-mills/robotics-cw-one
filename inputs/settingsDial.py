25c40c5cb66dfad64e0571b54853504eddcb4a7e
Saimpreet Singh <psyss22@nottingham.ac.uk>
2026-04-20 01:13:45 +0100

    Forgot to add the rotary encoder script

---
+++ inputs/settingsDial.py
@@ -0,0 +1,26 @@
+import grovepi
+
+
+class SettingsDial:
+
+    def __init__(self):
+        
+        # Assumes that the port is D2. Should use D2 since rotary motor requires two ports and D2 is the recommended port
+        grovepi.encoder_en()
+
+        self.last_position: int = grovepi.encoderRead()
+
+    
+    def get_rotation(self) -> int:
+
+        current_position: int = grovepi.encoderRead()
+        rotation_value = 0
+
+        if current_position > self.last_position :
+            rotation_value = 1
+        elif current_position < self.last_position:
+            rotation = -1
+
+        self.last_position = current_position
+
+        return rotation_value
