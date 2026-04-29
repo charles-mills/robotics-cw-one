786effaeba6b4ebf386232e8f105ffd1bbcf6c31
Saimpreet Singh <psyss22@nottingham.ac.uk>
2026-04-29 11:45:00 +0100

    buzzer file

---
+++ outputs/buzzer.py
@@ -0,0 +1,48 @@
+import grovepi
+
+from managers import AlertManager
+from managers.alert_manager import Alert, AlertType
+
+class Buzzer:
+
+    def __init__(self, port, alert_manager: AlertManager) -> None:
+        self.port = port
+        self.alert_manager: AlertManager = alert_manager
+
+
+        grovepi.pinMode(self.port, "OUTPUT")
+
+
+    @property
+    def sound_state(self) -> bool:
+        """
+
+        Returns True if the buzzer is making noise.
+
+        Returns:
+            bool: if the buzzer is on then it returns true otherwise, false.
+        """
+        return grovepi.digitalRead(self.port) == 1
+
+
+    @sound_state.setter
+    def sound_state(self, is_on: bool) -> None:
+        """
+
+        Turns the buzzer on or off.
+
+        Args:
+            is_on (bool): bool value indicating if the buzzer should be on or off.
+        
+        Returns:
+            None
+        """
+
+        if is_on:
+            grovepi.digitalWrite(self.port, 1)
+        else:
+            grovepi.digitalWrite(self.port, 0)
+
+    
+    def tick(self):
+        self.sound_state = self.alert_manager.total_alert > 0
