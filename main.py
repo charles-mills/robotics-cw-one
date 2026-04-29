57a4f0dab66f18ccbf25e1bfef157887f3058e25
Saimpreet Singh <psyss22@nottingham.ac.uk>
2026-04-29 20:05:43 +0100

    updated the ports

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ class Main:
     def __init__(self):
         self.alert_manager = AlertManager()
 
-        self.ultrasonic = Ultrasonic(1, self.alert_manager)
-        self.dht = Dht(2, self.alert_manager)
-        self.led = Led(3, self.alert_manager)
-        self.fan = Fan(4, self.alert_manager)
+        self.ultrasonic = Ultrasonic(2, self.alert_manager)
+        self.dht = Dht(3, self.alert_manager)
+        self.led = Led(4, self.alert_manager)
+        self.fan = Fan(5, self.alert_manager)
 
         self.settings_dial = SettingsDial()
         self.lcd = Lcd(self.alert_manager, self.dht, self.settings_dial)
-        self.select_btn = SelectButton(5, self.lcd)
-        self.cycle_btn = CycleButton(6, self.lcd)
-        self.buzzer = Buzzer(7, self.alert_manager)
+        self.select_btn = SelectButton(6, self.lcd)
+        self.cycle_btn = CycleButton(7, self.lcd)
+        self.buzzer = Buzzer(8, self.alert_manager)
 
         self.components = [self.ultrasonic, self.dht, self.cycle_btn, self.select_btn, self.led, self.fan, self.lcd, self.buzzer]
 
